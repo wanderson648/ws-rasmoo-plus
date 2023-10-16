9abbb45b12b9eea9200cc4399769c52b2a4b33ae
wanderson648 <testepedroca@gmail.com>
2023-10-16 18:46:39 -0300

    implementa busca por id e trata possiveis erros

--- src/main/java/com/client/ws/rasmooplus/controller/SubscriptionTypeController.java
+++ src/main/java/com/client/ws/rasmooplus/controller/SubscriptionTypeController.java
@@ -1,16 +1,17 @@
 package com.client.ws.rasmooplus.controller;
 
 import com.client.ws.rasmooplus.model.SubscriptionType;
-import com.client.ws.rasmooplus.repository.SubscriptionTypeRepository;
 import com.client.ws.rasmooplus.service.SubscriptionTypeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/subscription-type")
@@ -22,5 +23,14 @@ public class SubscriptionTypeController {
     @GetMapping
     public ResponseEntity<List<SubscriptionType>> findAll() {
         return ResponseEntity.status(HttpStatus.OK).body(subscriptionTypeService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SubscriptionType> findById(@PathVariable Long id) {
+        SubscriptionType subscriptionType = subscriptionTypeService.findById(id);
+        if(Objects.nonNull(subscriptionType)){
+            return ResponseEntity.status(HttpStatus.OK).body(subscriptionType);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
     }
 }
---
+++ src/main/java/com/client/ws/rasmooplus/service/applicationService/ApplicationSubscriptionTypeService.java
@@ -0,0 +1,44 @@
+package com.client.ws.rasmooplus.service.applicationService;
+
+import com.client.ws.rasmooplus.model.SubscriptionType;
+import com.client.ws.rasmooplus.repository.SubscriptionTypeRepository;
+import com.client.ws.rasmooplus.service.SubscriptionTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ApplicationSubscriptionTypeService implements SubscriptionTypeService {
+
+    private final SubscriptionTypeRepository subscriptionTypeRepository;
+
+    public ApplicationSubscriptionTypeService(SubscriptionTypeRepository subscriptionTypeRepository) {
+        this.subscriptionTypeRepository = subscriptionTypeRepository;
+    }
+    @Override
+    public List<SubscriptionType> findAll() {
+        return subscriptionTypeRepository.findAll();
+    }
+
+    @Override
+    public SubscriptionType findById(Long id) {
+        Optional<SubscriptionType> optionalSubscriptionType = subscriptionTypeRepository.findById(id);
+        return optionalSubscriptionType.orElse(null);
+    }
+
+    @Override
+    public SubscriptionType create(SubscriptionType subscriptionType) {
+        return null;
+    }
+
+    @Override
+    public SubscriptionType update(Long id, SubscriptionType subscriptionType) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
